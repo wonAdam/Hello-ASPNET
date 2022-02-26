46958479b912c1085529000f0ca1caf135058b7e
원동현 <ehdgus5500@gmail.com>
2022-02-26 15:37:00 +0900

    Add project files.

---
+++ Hello-ASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hello_ASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
