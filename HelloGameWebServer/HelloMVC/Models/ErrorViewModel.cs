864d1955e22a9d8697881f20421b3ccf9f792cd8
원동현 <ehdgus5500@gmail.com>
2022-02-27 10:28:33 +0900

    Blazor .. 필요없을듯

--- HelloGameWebServer/HelloMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HelloMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
